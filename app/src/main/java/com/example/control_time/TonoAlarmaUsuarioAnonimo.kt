34a32e11be8392407447e02d97e53cabd7bf0e76
andres.cantor@payu.com <andres.cantor@payu.com>
2023-03-16 10:43:35 -0500

    add tono screen

---
+++ app/src/main/java/com/example/control_time/TonoAlarmaUsuarioAnonimo.kt
@@ -0,0 +1,11 @@
+package com.example.control_time
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TonoAlarmaUsuarioAnonimo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tono_alarma_usuario_anonimo)
+    }
+}
